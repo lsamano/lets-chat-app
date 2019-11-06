079b6dbaa5a2596860d103ed8592ea223a979baa
lsamano <leizl.samano@flatironschool.com>
2019-11-06 10:43:05 -0500

    added env mongoose and mongodb

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './router';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const logger = require('morgan');
 
 const app = express()
+
+dotenv.config()
+const API_PORT = process.env.PORT || 3000
+const DB_URI = process.env.DB_URI
+
+mongoose.connect(DB_URI, { useNewUrlParser: true })
+const db = mongoose.connection
 
 app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,8 +20,6 @@ app.use(bodyParser.json())
 
 app.use('/api', router)
 
-const PORT = 3001
-
 app.get('/home', (req, res) => res.send({ hi: "hello" }))
 
-app.listen(PORT, () => console.log(`Running on Port ${PORT}`))
+app.listen(API_PORT, () => console.log(`Running on Port ${API_PORT}`))
